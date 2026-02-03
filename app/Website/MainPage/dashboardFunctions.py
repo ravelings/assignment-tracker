473427021f9e88cd0520cdcbf1ec692152441194
ravelings <chowemmeline@gmail.com>
2026-02-03 17:02:14 -0500

    Fixed typo "due_at" -> "due"

--- app/Website/MainPage/dashboardFunctions.py
+++ app/Website/MainPage/dashboardFunctions.py
@@ -4,7 +4,7 @@ def getPendingTasks(assignments: list) -> int:
     pending_tally = 0
     now = datetime.datetime.now(tz=datetime.timezone.utc)
     for assignment in assignments:
-        due = datetime.datetime.fromisoformat(assignment['due_at'])
+        due = datetime.datetime.fromisoformat(assignment['due'])
         if due - now < datetime.timedelta(hours=168):
             pending_tally += 1
     return pending_tally
